77765b8db638fc4fcb35d9a6620b79118c214b5e
Maxim Bezrukavnikov <dmntr@rambler.ru>
2023-07-03 21:47:26 +1000

    Google OAuth token autorefresh

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -25,8 +25,13 @@ class Controller extends BaseController
     public function list(Request $request): array
     {
         $params = $request->all();
+        try {
+            $this->checkOrUpdateToken();
 
-        return Event::get()->toArray();
+            return Event::get()->toArray();
+        } catch (Exception $exception) {
+            return json_decode($exception->getMessage(), true) ?? ['status' => 'Not found'];
+        }
     }
 
     /**
@@ -38,6 +43,8 @@ class Controller extends BaseController
         $params = $request->all();
 
         try {
+            $this->checkOrUpdateToken();
+
             if (!empty($params['start']) && !empty($params['end'])) {
                 $start = Carbon\Carbon::parse($params['start']);
                 $end = Carbon\Carbon::parse($params['end']);
@@ -58,7 +65,13 @@ class Controller extends BaseController
      */
     public function find($id): array
     {
-        return (array)Event::find($id);
+        try {
+            $this->checkOrUpdateToken();
+
+            return (array)Event::find($id);
+        } catch (Exception $exception) {
+            return json_decode($exception->getMessage(), true) ?? ['status' => 'Not found'];
+        }
     }
 
     /**
@@ -69,6 +82,8 @@ class Controller extends BaseController
     {
         $params = $request->all();
         try {
+            $this->checkOrUpdateToken();
+
             // create a new event
             $event = Event::create([
                 'name' => $params['name'],
@@ -84,6 +99,7 @@ class Controller extends BaseController
         }
 
         return [
+            'id' => $event->googleEvent->id,
             'summary' => $event->googleEvent->summary,
             'description' => $event->googleEvent->description,
             'start' => $event->googleEvent->getStart()->dateTime,
@@ -99,12 +115,14 @@ class Controller extends BaseController
     public function update(Request $request): array
     {
         $params = $request->all();
-        $events = Event::get();
-        // TODO
-        $firstEvent = $events->first();
-        $firstEvent->name = 'updated name';
 
         try {
+            $this->checkOrUpdateToken();
+
+            $events = Event::get();
+            // TODO
+            $firstEvent = $events->first();
+            $firstEvent->name = 'updated name';
             $firstEvent->save();
 
             $firstEvent->update(['name' => 'updated again']);
@@ -129,6 +147,8 @@ class Controller extends BaseController
         $params = $request->all();
 
         try {
+            $this->checkOrUpdateToken();
+
             $events = Event::get();
             // TODO
             $firstEvent = $events->first();
@@ -148,13 +168,11 @@ class Controller extends BaseController
 
     public function getClient(Request $request)
     {
-        $client = new Google_Client();
-        $client->setApplicationName('Google Sheets API PHP Quickstart');
-        $client->setScopes(Google_Service_Calendar::CALENDAR);
-        $client->setScopes('https://www.googleapis.com/auth/calendar');
-        $client->setAuthConfig(storage_path('app/google-calendar/oauth-credentials.json'));
-        $client->setAccessType('offline');
-        $client->setRedirectUri('https://dev4.pay2me.com/calendar-manager/public/api/get-client');
+        try {
+            $client = $this->getGoogleClient();
+        } catch (Exception $exception) {
+            return json_decode($exception->getMessage(), true);
+        }
         $credentialsPath = storage_path('app/google-calendar/oauth-token.json');
         if (file_exists($credentialsPath)) {
             $accessToken = json_decode(
@@ -177,27 +195,11 @@ class Controller extends BaseController
                 mkdir(dirname($credentialsPath), 0700, true);
             }
             file_put_contents($credentialsPath, json_encode($accessToken, JSON_PRETTY_PRINT));
-            //return ['saved' => $credentialsPath];
             return redirect('/api/get-client');
         }
         $client->setAccessToken($accessToken);
         if ($client->isAccessTokenExpired()) {
-            $jsonCred = file_get_contents($credentialsPath);
-            $jsonArray = json_decode($jsonCred, true);
-            if (empty($jsonArray['refresh_token'])) {
-                File::delete($credentialsPath);
-                //return [
-                //    'result' => 'error',
-                //    'message' => 'refresh_token needed! Please refresh page',
-                //];
-                return redirect('/api/get-client');
-            }
-            $client->fetchAccessTokenWithRefreshToken(
-                $jsonArray['refresh_token']
-            );
-            $newAccessToken = $client->getAccessToken();
-            $accessToken = array_merge($jsonArray, $newAccessToken);
-            file_put_contents($credentialsPath, json_encode($accessToken));
+            return $this->updateToken($credentialsPath, $client);
         }
         return [
             'result' => 'success',
@@ -210,5 +212,65 @@ class Controller extends BaseController
         return $request->all();
         //2023-06-26 17:30:00
         $date = Carbon::createFromFormat('m/d/Y', $myDate)->format('Y-m-d');
+    }
+
+    /**
+     * @return Google_Client
+     * @throws \Google\Exception
+     */
+    private function getGoogleClient(): Google_Client
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('Google Sheets API PHP Quickstart');
+        $client->setScopes(Google_Service_Calendar::CALENDAR);
+        $client->setAuthConfig(storage_path('app/google-calendar/oauth-credentials.json'));
+        $client->setAccessType('offline');
+        $client->setRedirectUri('https://dev4.pay2me.com/calendar-manager/public/api/get-client');
+        return $client;
+    }
+
+    /**
+     * @param string $credentialsPath
+     * @param Google_Client $client
+     * @return array
+     */
+    private function updateToken(string $credentialsPath, Google_Client $client): array
+    {
+        $jsonCred = file_get_contents($credentialsPath);
+        $jsonArray = json_decode($jsonCred, true);
+        if (empty($jsonArray['refresh_token'])) {
+            File::delete($credentialsPath);
+            //return [
+            //    'result' => 'error',
+            //    'message' => 'refresh_token needed! Please refresh page',
+            //];
+            return [
+                'result' => 'failed',
+                'message' => 'token is EXPIRED, refresh this page',
+            ];
+        }
+        $client->fetchAccessTokenWithRefreshToken(
+            $jsonArray['refresh_token']
+        );
+        $newAccessToken = $client->getAccessToken();
+        $accessToken = array_merge($jsonArray, $newAccessToken);
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        return [
+            'result' => 'success',
+            'message' => 'token is UPDATED',
+            'accessToken' => $accessToken,
+        ];
+    }
+
+    /**
+     * @throws \Google\Exception
+     */
+    private function checkOrUpdateToken(): void
+    {
+        $client = $this->getGoogleClient();
+        if ($client->isAccessTokenExpired()) {
+            $credentialsPath = storage_path('app/google-calendar/oauth-token.json');
+            $this->updateToken($credentialsPath, $client);
+        }
     }
 }
