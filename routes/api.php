7f61ff49d10aabd605748fffb84c56f5b6ff8afa
Maxim Bezrukavnikov <dmntr@rambler.ru>
2024-05-17 13:50:02 +1000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,9 +25,12 @@ Route::get('/events/list', 'Controller@list');
 Route::get('/events/find/{id}', 'Controller@find');
 Route::get('/events/update', 'Controller@update');
 Route::get('/events/delete', 'Controller@delete');
+Route::get('/events/clear', 'Controller@clear');
 
 Route::get('/get-client', 'Controller@getClient');
 Route::get('/json', 'Controller@json');
+Route::get('/force-update-token', 'Controller@forceUpdateToken');
+Route::get('/force-update-token/{count}', 'Controller@forceUpdateToken');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
